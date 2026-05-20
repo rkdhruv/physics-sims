568d017e4e9434a731e9770e57b13aa515b230c6
Dhruv Patel <dhruvpat@udel.edu>
2026-05-20 17:09:57 -0400

    stacked values into arrays indexed by body

--- solar-system/main.py
+++ solar-system/main.py
@@ -7,13 +7,12 @@ import matplotlib.pyplot as plt
 # G = 4*pi^2 follows from kepler's 3rd law (T=1 yr, a=1, M=1 M_sun from Earth).
 G = 4 * np.pi**2
 
-M_sun = 1.0
-M_earth = 3.0e-6 # TODO: update this value to be more accurate once more planets are added.
-
-# initial conditions\
-# Earth
-earth_pos = np.array([1.0, 0.0])
-earth_vel = np.array([0.0, 2 * np.pi])  # 2*pi AU/year — circular orbit speed at r=1 AU
-sun_pos = np.array([0.0, 0.0])
-sun_vel = np.array([0.0, 0.0]) # stationary for now
-
+positions = np.array([
+    [0.0, 0.0],         # Sun [0]
+    [1.0, 0.0],         # Earth [1]
+])
+velocities = np.array([
+    [0.0, 0.0],         # Sun [0]
+    [0.0, 2.0 * np.pi]  # Earth [1]
+])
+masses = np.array([1.0, 3.003e-6])  # Sun [0], Earth [1]
