f4daf9996973e6a0b986bb9b483988116b56822a
Dhruv Patel <dhruvpat@udel.edu>
2026-05-21 02:31:12 -0400

    added compute acceleration function

--- solar-system/main.py
+++ solar-system/main.py
@@ -16,3 +16,15 @@ velocities = np.array([
     [0.0, 2.0 * np.pi]  # Earth [1]
 ])
 masses = np.array([1.0, 3.003e-6])  # Sun [0], Earth [1]
+
+def compute_accelerations(positions, masses):
+    n = len(masses)
+    accelerations = np.zeros_like(positions)
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                r_vec = positions[j] - positions[i]
+                r_mag = np.linalg.norm(r_vec)
+                mass_factor = masses[j]
+                accelerations[i] += G * mass_factor * r_vec / r_mag**3
+    return accelerations
